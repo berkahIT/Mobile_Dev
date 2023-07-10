316a4d745e69e51457ecb4e1bde71cb44bb09b4a
Hayato <khayatullahalamin@gmail.com>
2023-07-10 21:29:27 +0800

    fix:home

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,11 +17,7 @@ class HomePage extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Image(
-                    image: Svg("assets/icons/User.svg", color: Colors.black),
-                    width: 28,
-                    height: 28,
-                  ),
+                  Icon(Icons.density_medium),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 109),
                     child: Text("Home",
@@ -67,9 +62,7 @@ class HomePage extends StatelessWidget {
               width: double.infinity,
               decoration: ShapeDecoration(
                   gradient: LinearGradient(
-                      begin: Alignment(-0.93, 0.36),
-                      end: Alignment(0.93, -0.36),
-                      colors: [Color(0xFF025464), Color(0x4940BBD4)]),
+                      colors: [Color(0xFF025464), Color(0xFF41BCD4)]),
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10))),
               child: Column(
@@ -160,65 +153,319 @@ class HomePage extends StatelessWidget {
                   fontWeight: FontWeight.w500),
             ),
             Padding(
-              padding: const EdgeInsets.only(top: 11),
+              padding: const EdgeInsets.only(top: 11, bottom: 31),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(bottom: 5, right: 53),
-                        width: 50,
-                        height: 50,
-                        decoration: ShapeDecoration(
+                  SizedBox(
+                    height: 75,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            margin: EdgeInsets.only(bottom: 5),
                             color: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8))),
-                        child: Center(
-                            child: Icon(Icons.arrow_upward_rounded,
-                                color: Color(0xFFA8BACD))),
-                      ),
-                      Text(
-                        "Transaksi",
-                        style: TextStyle(
-                            color: Color(0xFF7C8894),
-                            fontSize: 10,
-                            fontFamily: "Poppins",
-                            fontWeight: FontWeight.w500),
-                      )
-                    ],
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Icon(
+                                  Icons.arrow_upward,
+                                  color: Color(0xFFFC1C9D1),
+                                )),
+                          ),
+                          Text(
+                            "Transaksi",
+                            style: TextStyle(
+                                color: Color(0xFF7C8894),
+                                fontSize: 10,
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.w500),
+                          )
+                        ]),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(bottom: 5),
-                        width: 50,
-                        height: 50,
-                        decoration: ShapeDecoration(
+                  SizedBox(
+                    height: 75,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            margin: EdgeInsets.only(bottom: 5),
                             color: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8))),
-                        child: Center(
-                            child: Icon(
-                          Icons.arrow_downward_rounded,
-                          color: Color(0xFFA8BACD),
-                        )),
-                      ),
-                      Text(
-                        "Pendapatan",
-                        style: TextStyle(
-                            color: Color(0xFF7C8894),
-                            fontSize: 10,
-                            fontFamily: "Poppins",
-                            fontWeight: FontWeight.w500),
-                      )
-                    ],
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Icon(
+                                  Icons.arrow_downward,
+                                  color: Color(0xFFFC1C9D1),
+                                )),
+                          ),
+                          Text(
+                            "Pendapatan",
+                            style: TextStyle(
+                                color: Color(0xFF7C8894),
+                                fontSize: 10,
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.w500),
+                          )
+                        ]),
+                  ),
+                  SizedBox(
+                    height: 75,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            margin: EdgeInsets.only(bottom: 5),
+                            color: Colors.white,
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Icon(
+                                  Icons.file_open_rounded,
+                                  color: Color(0xFFFC1C9D1),
+                                )),
+                          ),
+                          Text(
+                            "Riwayat",
+                            style: TextStyle(
+                                color: Color(0xFF7C8894),
+                                fontSize: 10,
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.w500),
+                          )
+                        ]),
+                  ),
+                  SizedBox(
+                    height: 75,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            margin: EdgeInsets.only(bottom: 5),
+                            color: Colors.white,
+                            child: Align(
+                                alignment: Alignment.center,
+                                child: Icon(
+                                  Icons.arrow_downward,
+                                  color: Color(0xFFFC1C9D1),
+                                )),
+                          ),
+                          Text(
+                            "Lainnya",
+                            style: TextStyle(
+                                color: Color(0xFF7C8894),
+                                fontSize: 10,
+                                fontFamily: "Poppins",
+                                fontWeight: FontWeight.w500),
+                          )
+                        ]),
                   )
                 ],
               ),
-            )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Transaksi",
+                  style: TextStyle(
+                      color: Color(0xFF303840),
+                      fontSize: 16,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w500),
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  color: Color(0xFFFC1C9D1),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 19),
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(8),
+                height: 60,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.white),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 32,
+                          height: 33,
+                          margin: EdgeInsets.only(right: 16),
+                          decoration: BoxDecoration(color: Color(0xFFF7F7F7)),
+                          child: Icon(
+                            Icons.home,
+                            color: Color(0xFFFC1C9D1),
+                          ),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Freelancer",
+                              style: TextStyle(
+                                  color: Color(0xFF303840),
+                                  fontFamily: "Popins",
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                  letterSpacing: 1),
+                            ),
+                            Text(
+                              "14 July 2023",
+                              style: TextStyle(
+                                  color: Color(0xFF7D8895),
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 10,
+                                  letterSpacing: 1),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "+Rp.3.000.000",
+                      style: TextStyle(
+                          color: Color(0xFF009688),
+                          fontSize: 12,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Poppins"),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(8),
+                height: 60,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    color: Colors.white),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 32,
+                          height: 33,
+                          margin: EdgeInsets.only(right: 16),
+                          decoration: BoxDecoration(color: Color(0xFFF7F7F7)),
+                          child: Icon(
+                            Icons.shop,
+                            color: Color(0xFFFC1C9D1),
+                          ),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Beli Skincare",
+                              style: TextStyle(
+                                  color: Color(0xFF303840),
+                                  fontFamily: "Popins",
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500,
+                                  letterSpacing: 1),
+                            ),
+                            Text(
+                              "15 July 2023",
+                              style: TextStyle(
+                                  color: Color(0xFF7D8895),
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 10,
+                                  letterSpacing: 1),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "-Rp.500.000",
+                      style: TextStyle(
+                          color: Color(0xFFDC143C),
+                          fontSize: 12,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Poppins"),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.all(8),
+              height: 60,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(6), color: Colors.white),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 32,
+                        height: 33,
+                        margin: EdgeInsets.only(right: 16),
+                        decoration: BoxDecoration(color: Color(0xFFF7F7F7)),
+                        child: Icon(
+                          Icons.shop,
+                          color: Color(0xFFFC1C9D1),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Beli Makanan",
+                            style: TextStyle(
+                                color: Color(0xFF303840),
+                                fontFamily: "Popins",
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 1),
+                          ),
+                          Text(
+                            "15 July 2023",
+                            style: TextStyle(
+                                color: Color(0xFF7D8895),
+                                fontWeight: FontWeight.w400,
+                                fontSize: 10,
+                                letterSpacing: 1),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "-Rp.500.000",
+                    style: TextStyle(
+                        color: Color(0xFFDC143C),
+                        fontSize: 12,
+                        letterSpacing: 1,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: "Poppins"),
+                  )
+                ],
+              ),
+            ),
           ]),
         ),
       ),
